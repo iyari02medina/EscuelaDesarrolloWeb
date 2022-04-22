7ce8fe2552f7ce5a080b4f42a0784da7c14681f5
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-04-22 10:46:55 -0500

    Deep copy en JS

--- 04-IntermedioPOO/copiar-objetos.js
+++ 04-IntermedioPOO/copiar-objetos.js
@@ -4,6 +4,9 @@ const obj1 = {
     c: {
         d: "d",
         e: "e"
+    },
+    editA(){
+        this.a = "AAAAA";
     }
 };
 
@@ -11,7 +14,9 @@ const stringifiedComplexObj = JSON.stringify(obj1);
 const obj2 = JSON.parse(stringifiedComplexObj);      
 //JSON (stringify-parse) no son capaz de poder copiar metodos dentro de objetos 
 
-// / / / / / / / / / / / /  RECURSIVIDAD ESTRUCTURA / / / / / / / / / / / / / / / / 
+// / / / / / / / / / / / /  RECURSIVIDAD  / / / / / / / / / / / / / / / / 
+
+// Estructura
 
 //function recursiva(){
 //    if(/* Validacion */){        // funciona con cualquier statement
@@ -22,6 +27,7 @@ const obj2 = JSON.parse(stringifiedComplexObj);
 //};
 
 // Statement normal
+
 const numeritos = [1,2,3,4,5,6,7,8,9,4,5,6,2,45,528,21,24,5,2];
 /* 
 let numerito = 0;
@@ -32,6 +38,7 @@ for(let index = 0; index < numeritos.length; index++){
 */
 
 // funcion recursiva
+
 function recursiva(numbersArray){
     if(numbersArray.length != 0){
         const firstNum = numbersArray[0];
@@ -40,3 +47,51 @@ function recursiva(numbersArray){
        return recursiva(numbersArray);
     }
 }
+
+// / / / / / / / / / / / / / /  Deep copy con recursividad / / / / / / / / / / / / / / / / 
+
+// 1. Crear funciones para ver  si es un objeto o un array 
+
+function isObject (subject){
+    return typeof subject == "object";
+}
+function isArray (subject){
+    return Array.isArray(subject);
+}
+
+// 2. funcion recursiva 
+
+function deepCopy(subject){
+    let copySubject;
+// 3. validar si es objeto o array 
+    const subjectIsObject = isObject(subject);
+    const subjectIsArray = isArray(subject);
+
+    if(subjectIsArray){
+        copySubject = [];
+    } else if (subjectIsObject) {
+        copySubject = {}; 
+    } else {
+        return copySubject;
+    }
+// 4. Ejecutar codigo por cada una de las propiedades dentro del elento 
+    for(key in subject){
+        const keyIsObject = isObject(subject[key]);
+// 5. validar si la propiedad dentro del elemento es un objeto o un array 
+        if(keyIsObject){
+// 6. funcion recursiva para cada uno de las propiedades
+            copySubject[key] = deepCopy(subject[key])
+        } else {
+            if(subjectIsArray){
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key]
+            }
+        }
+    }
+
+    return copySubject;
+}
+
+
+

80eaefaa66ad50c26a13118b6eb0dbf9b7ad4234
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-05-24 09:34:37 -0500

    map reloaded

--- 05-manipulacion-arrays/curso-manipulacion-de-arrays-2-for-each/server/03-map-reloaded.js
+++ 05-manipulacion-arrays/curso-manipulacion-de-arrays-2-for-each/server/03-map-reloaded.js
@@ -24,9 +24,18 @@ console.log('original', orders);
 const rta = orders.map(item =>item.total);
 console.log(rta);
 
-const rta2 = orders.map(item =>{
-    item.tax = 0.19;
-    return item;
+// const rta2 = orders.map(item =>{
+//     item.tax = 0.19;
+//     return item;
+// });
+// console.log('rta2', rta2);
+// console.log('original', orders);
+const rta3 = orders.map((item) => {
+    // se hace de esta forma para que la constante "rta3" sea una nueva variable y no solo copie en memoria a la constante original.
+    return {
+        ... item,
+        tax: .19,
+    };
 });
-console.log('rta2', rta2);
-console.log('original', orders)
+console.log("rta3", rta3);
+console.log("original", orders);

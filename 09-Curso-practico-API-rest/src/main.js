7a7e75ff94aaab7dbeecd60eb4fe2e90e810b430
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-08-09 10:15:55 -0500

    Curso Practico de API REST

---
+++ 09-Curso-practico-API-rest/src/main.js
@@ -0,0 +1,6 @@
+async function getTrendingMoviesPreview (){
+    const res = await fetch(
+      "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
+    );
+    const data = await res.json();
+}
